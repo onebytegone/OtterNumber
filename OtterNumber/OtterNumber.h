61cddf392e00d31c82292213221aff33e89aeff1
Ethan Smith <ethan@onebytegone.com>
2015-06-12 22:25:16 -0400

    Initial Commit

---
+++ OtterNumber/OtterNumber.h
@@ -0,0 +1,19 @@
+//
+//  OtterNumber.h
+//  OtterNumber
+//
+//  Created by Ethan Smith on 6/12/15.
+//  Copyright (c) 2015 Ethan Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OtterNumber.
+FOUNDATION_EXPORT double OtterNumberVersionNumber;
+
+//! Project version string for OtterNumber.
+FOUNDATION_EXPORT const unsigned char OtterNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OtterNumber/PublicHeader.h>
+
+
